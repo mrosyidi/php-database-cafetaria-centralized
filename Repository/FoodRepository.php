f46bb0d995c3e5121e0bf0594d5b85cd904890ca
mrosyidi <rosyidiyuekunchen@gmail.com>
2024-12-27 10:37:13 +0700

    add FoodRepository.php

---
+++ Repository/FoodRepository.php
@@ -0,0 +1,51 @@
+<?php 
+
+    namespace Repository 
+    {
+        use Entity\Food;
+
+        interface FoodRepository 
+        {
+            public function findAll(): array;
+            public function save(Food $food): void;
+            public function remove(int $number): bool;
+        }
+
+        class FoodRepositoryImpl implements FoodRepository 
+        {
+            private \PDO $connection;
+
+            public function __construct(\PDO $connection)
+            {
+                $this->connection = $connection;
+            }
+
+            public function findAll(): array 
+            {
+                $sql = "SELECT name, price FROM foods";
+                $statement = $this->connection->prepare($sql);
+                $statement->execute();
+                $foods = [];
+
+                foreach($statement as $row)
+                {
+                    $food = new Food();
+                    $food->setName($row['name']);
+                    $food->setPrice($row['price']);
+                    $foods[] = $food;
+                }
+
+                return $foods;
+            }
+
+            public function save(Food $food): void 
+            {
+
+            }
+
+            public function remove(int $number): bool 
+            {
+
+            }
+        }
+    }
