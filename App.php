a148a2b54880ad99af967112eab5ece5813e03dc
mrosyidi <rosyidiyuekunchen@gmail.com>
2024-12-27 10:45:56 +0700

    update App.php require FoodRepository.php

--- App.php
+++ App.php
@@ -2,8 +2,10 @@
 
     require_once __DIR__ . "/Config/Database.php";
     require_once __DIR__ . "/Entity/Food.php";
+    require_once __DIR__ . "/Repository/FoodRepository.php";
 
     use Config\Database;
     use Entity\Food;
+    use Repository\FoodRepositoryImpl;
 
     echo "Cafetaria App" . PHP_EOL;
