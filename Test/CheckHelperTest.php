ec67b4adaef028222104b846398a974df565def5
mrosyidi <rosyidiyuekunchen@gmail.com>
2024-12-28 21:55:39 +0700

    add CheckHelperTest.php

---
+++ Test/CheckHelperTest.php
@@ -0,0 +1,21 @@
+<?php 
+
+    require_once __DIR__ . "/../Config/Database.php";
+    require_once __DIR__ . "/../Entity/Food.php";
+    require_once __DIR__ . "/../Repository/FoodRepository.php";
+    require_once __DIR__ . "/../Helper/CheckHelper.php";
+
+    use Config\Database;
+    use Repository\FoodRepositoryImpl;
+    use Helper\CheckHelper;
+
+    function testCheckHelperFoodNotFound(): void
+    {
+        $connection = Database::getConnection();
+        $foodRepository = new FoodRepositoryImpl($connection);
+        $foods = $foodRepository->findAll();
+        $result = CheckHelper::check($foods, "Nasi Kuning");
+        var_dump($result);
+    }
+
+    testCheckHelperFoodNotFound();
