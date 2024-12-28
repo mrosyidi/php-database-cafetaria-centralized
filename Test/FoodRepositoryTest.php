91fbd8023fd99d223ab4f3c6dff9a2518a2faf20
mrosyidi <rosyidiyuekunchen@gmail.com>
2024-12-28 14:27:40 +0700

    update FoodRepositoryTest.php menambah makanan

--- Test/FoodRepositoryTest.php
+++ Test/FoodRepositoryTest.php
@@ -4,15 +4,26 @@
     require_once __DIR__ . "/../Entity/Food.php";
     require_once __DIR__ . "/../Repository/FoodRepository.php";
 
+    use Config\Database;
     use Entity\Food;
     use Repository\FoodRepositoryImpl;
 
     function testFindAll(): void 
     {
-        $connection = \Config\Database::getConnection();
+        $connection = Database::getConnection();
         $foodRepository = new FoodRepositoryImpl($connection);
         $foods = $foodRepository->findAll();
         var_dump($foods);
     }
 
-    testFindAll();
+    function testSave(): void 
+    {
+        $food = new Food("Ayam Panggang", 15000);
+        $connection = Database::getConnection();
+        $foodRepository = new FoodRepositoryImpl($connection);
+        $foodRepository->save($food);
+        $foods = $foodRepository->findAll();
+        var_dump($foods);
+    }
+
+    testSave();
