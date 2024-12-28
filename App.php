ed18fb83294c1f470ad415cbbdeb2e07ff2abb58
mrosyidi <rosyidiyuekunchen@gmail.com>
2024-12-28 10:45:30 +0700

    update App.php menampilkan menu utama

--- App.php
+++ App.php
@@ -11,3 +11,41 @@
     use Helper\InputHelper;
 
     echo "Cafetaria App" . PHP_EOL;
+
+    while(true)
+    {
+        echo "MENU UTAMA" . PHP_EOL;
+        echo "1. Daftar Makanan" . PHP_EOL;
+        echo "2. Daftar Minuman" . PHP_EOL;
+        echo "3. Pemesanan" . PHP_EOL;
+        echo "4. Pembayaran" . PHP_EOL;
+        echo "5. Detail" . PHP_EOL;
+        echo "x. Keluar" . PHP_EOL;
+
+        $pilihan = InputHelper::input("Pilih");
+
+        if($pilihan == "1")
+        {
+
+        }else if($pilihan == "2")
+        {
+
+        }else if($pilihan == "3")
+        {
+
+        }else if($pilihan == "4")
+        {
+
+        }else if($pilihan == "5")
+        {
+
+        }else if($pilihan == "x")
+        {
+            break;
+        }else 
+        {
+            echo "Pilihan tidak dimengerti" . PHP_EOL;
+        }
+    }
+
+    echo "Sampai Jumpa Lagi" . PHP_EOL;
