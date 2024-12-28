7f747e721de1887cad1cc39eeb9a867407a66191
mrosyidi <rosyidiyuekunchen@gmail.com>
2024-12-28 13:47:38 +0700

    add FoodServiceTest.php

---
+++ Test/FoodServiceTest.php
@@ -0,0 +1,21 @@
+<?php 
+
+    require_once __DIR__ . "/../Config/Database.php";
+    require_once __DIR__ . "/../Entity/Food.php";
+    require_once __DIR__ . "/../Repository/FoodRepository.php";
+    require_once __DIR__ . "/../Service/FoodService.php";
+
+    use Config\Database;
+    use Entity\Food;
+    use Repository\FoodRepositoryImpl;  
+    use Service\FoodServiceImpl;
+
+    function testShowFood(): void
+    {
+        $connection = Database::getConnection();
+        $foodRepository = new FoodRepositoryImpl($connection);
+        $foodService = new FoodServiceImpl($foodRepository);
+        $foodService->showFood();
+    }
+
+    testShowFood();
