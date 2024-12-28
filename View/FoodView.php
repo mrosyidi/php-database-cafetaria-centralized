fb0945082bcc28c331de396dac48a60f2ea9cb97
mrosyidi <rosyidiyuekunchen@gmail.com>
2024-12-28 14:00:23 +0700

    add FoodView.php

---
+++ View/FoodView.php
@@ -0,0 +1,46 @@
+<?php 
+
+    namespace View 
+    {
+        use Service\FoodService;
+        use Helper\InputHelper;
+
+        class FoodView 
+        {
+            private FoodService $foodService;
+
+            public function __construct(FoodService $foodService)
+            {
+                $this->foodService = $foodService;
+            }
+
+            public function showFood(): void 
+            {
+                while(true)
+                {
+                    $this->foodService->showFood();
+
+                    echo "Menu Makanan" . PHP_EOL;
+                    echo "1. Tambah Makanan" . PHP_EOL;
+                    echo "2. Hapus Makanan" . PHP_EOL;
+                    echo "x. Kembali" . PHP_EOL;
+
+                    $pilihan = InputHelper::input("Pilih");
+
+                    if($pilihan == "1")
+                    {
+
+                    }else if($pilihan == "2")
+                    {
+                        
+                    }else if($pilihan == "x")
+                    {
+                        break;
+                    }else 
+                    {
+                        echo "Pilihan tidak dimengerti" . PHP_EOL;
+                    }
+                }
+            }
+        }
+    }
