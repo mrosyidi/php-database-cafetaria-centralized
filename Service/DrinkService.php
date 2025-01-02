140f16b3c2f64dd75d911519bc6c939eca14f779
mrosyidi <rosyidiyuekunchen@gmail.com>
2025-01-02 15:11:35 +0700

    add DrinkService.php

---
+++ Service/DrinkService.php
@@ -0,0 +1,59 @@
+<?php 
+
+    namespace Service 
+    {
+        use Entity\Drink;
+        use Repository\DrinkRepository;
+
+        interface DrinkService 
+        {
+            public function showDrink(): void;
+            public function addDrink(string $name, int $price): void;
+            public function getDrink(): array;
+            public function removeDrink(string $name): bool;
+        }
+
+        class DrinkServiceImpl implements DrinkService 
+        {
+            private DrinkRepository $drinkRepository;
+
+            public function __construct(DrinkRepository $drinkRepository)
+            {
+                $this->drinkRepository = $drinkRepository;
+            }
+
+            public function showDrink(): void
+            {
+                echo "DAFTAR MINUMAN" . PHP_EOL;
+
+                $drinks = $this->drinkRepository->findAll();
+
+                if(empty($drinks))
+                {
+                    echo "Tidak ada daftar minuman" . PHP_EOL;
+                }else
+                {
+                    foreach($drinks as $number => $drink)
+                    {
+                        $number++;
+                        echo "$number. " . $drink->getName() . "  Rp." . $drink->getPrice() . PHP_EOL;
+                    }
+                }
+            }
+
+            public function addDrink(string $name, int $price): void
+            {
+
+            }
+
+            public function getDrink(): array
+            {
+
+            }
+
+            public function removeDrink(string $name): bool
+            {
+
+            }
+        }
+    }
