5aef3378d106a23819c6f4fe9d3053841d91c905
mrosyidi <rosyidiyuekunchen@gmail.com>
2025-01-02 16:36:06 +0700

    add DrinkViewTest.php

---
+++ Test/DrinkViewTest.php
@@ -0,0 +1,25 @@
+<?php 
+
+    require_once __DIR__ . "/../Config/Database.php";
+    require_once __DIR__ . "/../Entity/Drink.php";
+    require_once __DIR__ . "/../Repository/DrinkRepository.php";
+    require_once __DIR__ . "/../Service/DrinkService.php";
+    require_once __DIR__ . "/../View/DrinkView.php";
+    require_once __DIR__ . "/../Helper/CheckHelper.php";
+    require_once __DIR__ . "/../Helper/InputHelper.php";
+
+    use Config\Database;
+    use Repository\DrinkRepositoryImpl;
+    use Service\DrinkServiceImpl;
+    use View\DrinkView;
+
+    function testViewShowDrink(): void 
+    {
+        $connection = Database::getConnection();
+        $drinkRepository = new DrinkRepositoryImpl($connection);
+        $drinkService = new DrinkServiceImpl($drinkRepository);
+        $drinkView = new DrinkView($drinkService);
+        $drinkView->showDrink();
+    }
+
+    testViewShowDrink();
