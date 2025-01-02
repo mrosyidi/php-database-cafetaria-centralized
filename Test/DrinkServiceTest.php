208dee3732c5e71194dba834a454628a4345d42e
mrosyidi <rosyidiyuekunchen@gmail.com>
2025-01-02 15:11:49 +0700

    add DrinkServiceTest.php

---
+++ Test/DrinkServiceTest.php
@@ -0,0 +1,21 @@
+<?php 
+
+    require_once __DIR__ . "/../Config/Database.php";
+    require_once __DIR__ . "/../Entity/Drink.php";
+    require_once __DIR__ . "/../Repository/DrinkRepository.php";
+    require_once __DIR__ . "/../Service/DrinkService.php";
+
+    use Config\Database;
+    use Entity\Drink;
+    use Repository\DrinkRepositoryImpl;  
+    use Service\DrinkServiceImpl;
+
+    function testShowDrink(): void
+    {
+        $connection = Database::getConnection();
+        $drinkRepository = new DrinkRepositoryImpl($connection);
+        $drinkService = new DrinkServiceImpl($drinkRepository);
+        $drinkService->showDrink();
+    }
+
+    testShowDrink();
