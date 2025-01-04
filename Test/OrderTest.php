082c3c9f5044169667e9ec58130194501af0b320
mrosyidi <rosyidiyuekunchen@gmail.com>
2025-01-04 21:38:20 +0700

    update OrderTest.php menambah method testInstanceOrderEncapsulate

--- Test/OrderTest.php
+++ Test/OrderTest.php
@@ -4,10 +4,21 @@
 
     use Entity\Order;
 
-    function testOrderCostruct(): void
+    function testInstanceOrderCostruct(): void
     {
         $order = new Order(1, "Mie Goreng", 6000, 2);
         var_dump($order);
     }
 
-    testOrderCostruct();
+    function testInstanceOrderEncapsulate(): void
+    {
+        $order = new Order();
+        $order->setCode(1);
+        $order->setName("Mie Goreng");
+        $order->setPrice(6000);
+        $order->setQty(2);
+        $order->setSubTotal();
+        var_dump($order);
+    }
+
+    testInstanceOrderEncapsulate();
