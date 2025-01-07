a039ebf0281671514f0aec946a44fd9506e8db32
mrosyidi <rosyidiyuekunchen@gmail.com>
2025-01-07 12:43:50 +0700

    add OrderService.php

---
+++ Service/OrderService.php
@@ -0,0 +1,60 @@
+<?php 
+
+    namespace Service 
+    {
+        use Entity\Order;
+        use Repository\OrderRepository;
+
+        interface OrderService
+        {
+            public function showOrder(): void;
+            public function addOrder(int $code, string $name, int $price, int $qty): void;
+            public function removeOrder(int $code): void;
+            public function getOrder(): array;
+        }
+
+        class OrderServiceImpl implements OrderService
+        {
+            private OrderRepository $orderRepository;
+
+            public function __construct(OrderRepository $orderRepository)
+            {
+                $this->orderRepository = $orderRepository;
+            }
+
+            public function showOrder(): void
+            {
+                $orders = $this->orderRepository->findAll();
+
+                echo "DAFTAR PESANAN" . PHP_EOL;
+
+                if(empty($orders))
+                {
+                    echo "Tidak ada daftar pesanan" . PHP_EOL;
+                }else
+                {
+                    foreach($orders as $number => $order)
+                    {
+                        $number++;
+                        echo "$number. " . $order->getCode() . " " . $order->getName() . " Rp." . $order->getPrice() .
+                        " (x" . $order->getQty() . ") Rp." . $order->getSubTotal() . PHP_EOL;
+                    }
+                }
+            }
+
+            public function addOrder(int $code, string $name, int $price, int $qty): void 
+            {
+
+            }
+
+            public function removeOrder(int $code): void
+            {
+
+            }
+
+            public function getOrder(): array
+            {
+
+            }
+        }
+    }
