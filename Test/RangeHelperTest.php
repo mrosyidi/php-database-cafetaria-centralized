3c98379625b5258b9e7ad2d319580589798466b2
mrosyidi <rosyidiyuekunchen@gmail.com>
2025-01-12 15:07:40 +0700

    update RangeHelperTest.php data yang akan dipesan ada pada range

--- Test/RangeHelperTest.php
+++ Test/RangeHelperTest.php
@@ -2,12 +2,14 @@
 
     require_once __DIR__ . "/../Config/Database.php";
     require_once __DIR__ . "/../Entity/Food.php";
+    require_once __DIR__ . "/../Entity/Drink.php";
     require_once __DIR__ . "/../Repository/FoodRepository.php";
     require_once __DIR__ . "/../Repository/DrinkRepository.php";
     require_once __DIR__ . "/../Helper/RangeHelper.php";
 
     use Config\Database;
     use Entity\Food;
+    use Entity\Drink;
     use Repository\FoodRepositoryImpl;
     use Repository\DrinkRepositoryImpl;
     use Helper\RangeHelper;
@@ -30,4 +32,13 @@
         var_dump($result);
     }
 
-    testRangeHelperOutOfRangePlus();
+    function testRangeHelperInRange(): void
+    {
+        $connection = Database::getConnection();
+        $drinkRepository = new DrinkRepositoryImpl($connection);
+        $drinks = $drinkRepository->findAll();
+        $result = RangeHelper::range($drinks, 2);
+        var_dump($result);
+    }
+
+    testRangeHelperInRange();
