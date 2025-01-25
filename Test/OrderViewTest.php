182aef363b9bdb4a60a098d40f6962fbedc570ed
mrosyidi <rosyidiyuekunchen@gmail.com>
2025-01-25 17:34:38 +0700

    update OrderViewTest.php menambah pemesanan makanan kode sama

--- Test/OrderViewTest.php
+++ Test/OrderViewTest.php
@@ -1,14 +1,27 @@
 <?php 
 
     require_once __DIR__ . "/../Config/Database.php";
+    require_once __DIR__ . "/../Entity/Food.php";
+    require_once __DIR__ . "/../Entity/Drink.php";
     require_once __DIR__ . "/../Entity/Order.php";
+    require_once __DIR__ . "/../Repository/FoodRepository.php";
+    require_once __DIR__ . "/../Repository/DrinkRepository.php";
     require_once __DIR__ . "/../Repository/OrderRepository.php";
+    require_once __DIR__ . "/../Service/FoodService.php";
+    require_once __DIR__ . "/../Service/DrinkService.php";
     require_once __DIR__ . "/../Service/OrderService.php";
     require_once __DIR__ . "/../View/OrderView.php";
+    require_once __DIR__ . "/../Helper/CodeHelper.php";
+    require_once __DIR__ . "/../Helper/DataHelper.php";
     require_once __DIR__ . "/../Helper/InputHelper.php";
+    require_once __DIR__ . "/../Helper/RangeHelper.php";
    
     use Config\Database;
+    use Repository\FoodRepositoryImpl;
+    use Repository\DrinkRepositoryImpl;
     use Repository\OrderRepositoryImpl;
+    use Service\FoodServiceImpl;
+    use Service\DrinkServiceImpl;
     use Service\OrderServiceImpl;
     use View\OrderView;
    
@@ -21,4 +34,18 @@
         $orderView->showOrder();
     }
 
-    testViewShowOrder();
+    function testViewAddOrderFoodSameCode(): void 
+    {
+        $connection = Database::getConnection();
+        $foodRepository = new FoodRepositoryImpl($connection);
+        $foodService = new FoodServiceImpl($foodRepository);
+        $drinkRepository = new DrinkRepositoryImpl($connection);
+        $drinkService = new DrinkServiceImpl($drinkRepository);
+        $orderRepository = new OrderRepositoryImpl($connection);
+        $orderService = new OrderServiceImpl($orderRepository);
+        $orderView = new OrderView($foodService, $drinkService, $orderService);
+        $orderView->addOrder(1, false);
+        $orderService->showOrder();
+    }
+
+    testViewAddOrderFoodSameCode();
