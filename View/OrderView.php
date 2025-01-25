f8a9203ef389feecabf58af08ea52cab44ce1d75
mrosyidi <rosyidiyuekunchen@gmail.com>
2025-01-25 17:33:28 +0700

    update OrderView.php menambah pemesanan

--- View/OrderView.php
+++ View/OrderView.php
@@ -2,15 +2,24 @@
 
     namespace View 
     {
+        use Service\FoodService;
+        use Service\DrinkService;
         use Service\OrderService;
+        use Helper\CodeHelper;
+        use Helper\DataHelper;
         use Helper\InputHelper;
+        use Helper\RangeHelper;
 
         class OrderView
         {
+            private FoodService $foodService;
+            private DrinkService $drinkService;
             private OrderService $orderService;
 
-            public function __construct(OrderService $orderService)
+            public function __construct(FoodService $foodService, DrinkService $drinkService, OrderService $orderService)
             {
+                $this->foodService = $foodService;
+                $this->drinkSerive = $drinkService;
                 $this->orderService = $orderService;
             }
 
@@ -39,6 +48,57 @@
                     }else
                     {
                         echo "Pilihan tidak dimengerti" . PHP_EOL;
+                    }
+                }
+            }
+
+            public function addOrder(int $numberOrder, bool $exit): void 
+            {
+                $orders = $this->orderService->getOrder();
+
+                if($numberOrder == 1)
+                {
+                    $order = "makanan";
+                    $items = $this->foodService->getFood();
+                    $this->foodService->showFood();
+                }else if($numberOrder == 2)
+                {
+                    $order = "minuman";
+                    $items = $this->drinkService->getDrink();
+                    $this->drinkService->showDrink();
+                }
+
+                echo "MENAMBAH PESANAN" . PHP_EOL;
+
+                $number = InputHelper::input("Nomor $order (x untuk batal)");
+
+                if($number == "x")
+                {
+                    echo "Batal menambah pesanan" . PHP_EOL;
+                }else if(!is_numeric($number))
+                {
+                    echo "Gagal menambah pesanan, nomor $order harus bilangan" . PHP_EOL;
+                }else if(!RangeHelper::range($items, $number))
+                {
+                    echo "Gagal menambah pesanan, tidak ada $order dengan nomor $number" . PHP_EOL;
+                }else
+                {
+                    $qty = InputHelper::input("Jumlah (x untuk batal)");
+
+                    if($qty == "x")
+                    {
+                        echo "Batal menambah $order" . PHP_EOL;
+                    }else if(!is_numeric($qty))
+                    {
+                        echo "Gagal menambah makanan, nomor $order harus bilangan" . PHP_EOL;
+                    }else if($qty <= 0)
+                    {
+                        echo "Gagal menambah makanan, jumlah $order minimal satu" . PHP_EOL;
+                    }else
+                    {
+                        $code = CodeHelper::code($orders, $exit);
+                        $item = DataHelper::data($items, $number-1);
+                        $this->orderService->addOrder($code, $item["name"], $item["price"], $qty);
                     }
                 }
             }
