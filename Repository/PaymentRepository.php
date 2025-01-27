7d7f80d49d73e87b1e4e79d1702a4be1ea7d924d
mrosyidi <rosyidiyuekunchen@gmail.com>
2025-01-27 12:35:50 +0700

    add PaymentRepository.php

---
+++ Repository/PaymentRepository.php
@@ -0,0 +1,47 @@
+<?php 
+
+    namespace Repository 
+    {
+        use Entity\Payment;
+
+        interface PaymentRepository
+        {
+            public function findAll(): array;
+            public function save(Payment $payment): void;
+        }
+
+        class PaymentRepositoryImpl implements PaymentRepository
+        {
+            private \PDO $connection;
+
+            public function __construct(\PDO $connection)
+            {
+                $this->connection = $connection;
+            }
+
+            public function findAll(): array 
+            {
+                $sql = "SELECT code,total,pay,changes FROM payments";
+                $statement = $this->connection->prepare($sql);
+                $statement->execute();
+                $payments = [];
+
+                foreach($statement as $row)
+                {
+                    $payment = new Payment();
+                    $payment->setCode($row['code']);
+                    $payment->setTotal($row['total']);
+                    $payment->setPay($row['pay']);
+                    $payment->setChange($row['changes']);
+                    $payments[] = $payment;
+                }
+
+                return $payments;
+            }
+
+            public function save(Payment $payment): void 
+            {
+                
+            } 
+        }
+    }
