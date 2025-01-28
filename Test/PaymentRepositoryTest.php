42f2178f9ea5914e808d782388540080c2bf1b9f
mrosyidi <rosyidiyuekunchen@gmail.com>
2025-01-28 14:28:13 +0700

    update PaymentRepositoryTest.php menambah data pembayaran

--- Test/PaymentRepositoryTest.php
+++ Test/PaymentRepositoryTest.php
@@ -5,6 +5,7 @@
     require_once __DIR__ . "/../Repository/PaymentRepository.php";
 
     use Config\Database;
+    use Entity\Payment;
     use Repository\PaymentRepositoryImpl;
 
     function testFindAll(): void 
@@ -15,4 +16,14 @@
         var_dump($payments);
     }
 
-    testFindAll();
+    function testSave(): void 
+    {
+        $connection = Database::getConnection();
+        $payment = new Payment(2,45000,50000,5000);
+        $paymentRepository = new PaymentRepositoryImpl($connection);
+        $paymentRepository->save($payment);
+        $payments = $paymentRepository->findAll();
+        var_dump($payments);
+    }
+
+    testSave();
