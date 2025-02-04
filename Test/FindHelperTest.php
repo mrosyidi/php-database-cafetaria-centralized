a3afe0addfe7c06dc4be3555e89235dc0845b5c5
mrosyidi <rosyidiyuekunchen@gmail.com>
2025-02-04 09:54:35 +0700

    add FindHelperTest.php

---
+++ Test/FindHelperTest.php
@@ -0,0 +1,21 @@
+<?php 
+
+    require_once __DIR__ . "/../Config/Database.php";
+    require_once __DIR__ . "/../Entity/Order.php";
+    require_once __DIR__ . "/../Repository/OrderRepository.php";
+    require_once __DIR__ . "/../Helper/FindHelper.php";
+
+    use Config\Database;
+    use Repository\OrderRepositoryImpl;
+    use Helper\FindHelper;
+
+    function testFindHelperNotFound(): void
+    {
+        $connection = Database::getConnection();
+        $orderRepository = new OrderRepositoryImpl($connection);
+        $orders = $orderRepository->findAll();
+        $found = FindHelper::find($orders, 2);
+        var_dump($found);
+    }
+
+    testFindHelperNotFound();
