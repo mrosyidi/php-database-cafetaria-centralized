c4cae85ccd2dbe033658fb9d5047073b378179ff
mrosyidi <rosyidiyuekunchen@gmail.com>
2025-02-04 12:11:13 +0700

    update PaymentViewTest.php menambah data pembayaran

--- Test/PaymentViewTest.php
+++ Test/PaymentViewTest.php
@@ -2,12 +2,15 @@
 
     require_once __DIR__ . "/../Config/Database.php";
     require_once __DIR__ . "/../Entity/Order.php";
+    require_once __DIR__ . "/../Entity/Payment.php";
     require_once __DIR__ . "/../Repository/OrderRepository.php";
     require_once __DIR__ . "/../Repository/PaymentRepository.php";
     require_once __DIR__ . "/../Service/OrderService.php";
     require_once __DIR__ . "/../Service/PaymentService.php";
     require_once __DIR__ . "/../View/PaymentView.php";
+    require_once __DIR__ . "/../Helper/FindHelper.php";
     require_once __DIR__ . "/../Helper/InputHelper.php";
+    require_once __DIR__ . "/../Helper/PayHelper.php";
 
     use Config\Database;
     use Repository\OrderRepositoryImpl;
@@ -27,4 +30,16 @@
         $paymentView->showPayment();
     }
 
-    testViewShowPayment();
+    function testViewAddPayment(): void
+    {
+        $connection = Database::getConnection();
+        $orderRepository = new OrderRepositoryImpl($connection);
+        $orderService = new OrderServiceImpl($orderRepository);
+        $paymentRepository = new PaymentRepositoryImpl($connection);
+        $paymentService = new PaymentServiceImpl($paymentRepository);
+        $paymentView = new PaymentView($orderService, $paymentService);
+        $orderService->showOrder();
+        $paymentView->addPayment();
+    }
+
+    testViewAddPayment();
