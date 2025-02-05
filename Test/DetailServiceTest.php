0aefddde38a3d92f6d308c90fd179e8dbb566756
mrosyidi <rosyidiyuekunchen@gmail.com>
2025-02-05 10:54:49 +0700

    update DetailServiceTest.php menambah data detail

--- Test/DetailServiceTest.php
+++ Test/DetailServiceTest.php
@@ -1,12 +1,16 @@
 <?php 
 
     require_once __DIR__ . "/../Config/Database.php";
+    require_once __DIR__ . "/../Entity/Order.php";
     require_once __DIR__ . "/../Entity/Detail.php";
+    require_once __DIR__ . "/../Repository/OrderRepository.php";
     require_once __DIR__ . "/../Repository/DetailRepository.php";
     require_once __DIR__ . "/../Service/DetailService.php";
 
     use Config\Database;
+    use Entity\Order;
     use Entity\Detail;
+    use Repository\OrderRepositoryImpl;
     use Repository\DetailRepositoryImpl;
     use Service\DetailServiceImpl;
 
@@ -18,4 +22,15 @@
         $detailService->showDetail(2);
     }
 
-    testShowDetail();
+    function testAddDetail(): void 
+    {
+        $connection = Database::getConnection();
+        $orderRepository = new OrderRepositoryImpl($connection);
+        $detailRepository = new DetailRepositoryImpl($connection);
+        $detailService = new DetailServiceImpl($detailRepository);
+        $orders = $orderRepository->findAll();
+        $detailService->addDetail($orders);
+        $detailService->showDetail(2);
+    }
+
+    testAddDetail();
