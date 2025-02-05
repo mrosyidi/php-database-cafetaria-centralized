7864f02e5f142f48cb74bfab44fca1cfa0283a4f
mrosyidi <rosyidiyuekunchen@gmail.com>
2025-02-05 07:57:12 +0700

    add DetailRepository.php

---
+++ Repository/DetailRepository.php
@@ -0,0 +1,48 @@
+<?php 
+
+    namespace Repository 
+    {
+        use Entity\Detail;
+
+        interface DetailRepository
+        {
+            public function findAll(): array;
+            public function save(Detail $detail): void;
+        }
+
+        class DetailRepositoryImpl implements DetailRepository
+        {
+            private \PDO $connection;
+
+            public function __construct(\PDO $connection)
+            {
+                $this->connection = $connection;
+            }
+
+            public function findAll(): array
+            {
+                $sql = "SELECT code, name, price, qty, sub_total FROM details";
+                $statement = $this->connection->prepare($sql);
+                $statement->execute();
+                $details = [];
+
+                foreach($statement as $row)
+                {
+                    $detail = new Detail();
+                    $detail->setCode($row['code']);
+                    $detail->setName($row['name']);
+                    $detail->setPrice($row['price']);
+                    $detail->setQty($row['qty']);
+                    $detail->setSubTotal($row['sub_total']);
+                    $details[] = $detail;
+                }
+
+                return $details;
+            }
+
+            public function save(Detail $detail): void
+            {
+
+            }
+        }
+    }
