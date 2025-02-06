fd96d728f70104ab3e5a7c358030ff9974313fe4
mrosyidi <rosyidiyuekunchen@gmail.com>
2025-02-06 09:53:31 +0700

    update PaymentViewTest.php mengubah intruksi testViewShowPayment

--- Test/PaymentViewTest.php
+++ Test/PaymentViewTest.php
@@ -5,8 +5,10 @@
     require_once __DIR__ . "/../Entity/Payment.php";
     require_once __DIR__ . "/../Repository/OrderRepository.php";
     require_once __DIR__ . "/../Repository/PaymentRepository.php";
+    require_once __DIR__ . "/../Repository/DetailRepository.php";
     require_once __DIR__ . "/../Service/OrderService.php";
     require_once __DIR__ . "/../Service/PaymentService.php";
+    require_once __DIR__ . "/../Service/DetailService.php";
     require_once __DIR__ . "/../View/PaymentView.php";
     require_once __DIR__ . "/../Helper/DuplicateHelper.php";
     require_once __DIR__ . "/../Helper/FindHelper.php";
@@ -16,8 +18,10 @@
     use Config\Database;
     use Repository\OrderRepositoryImpl;
     use Repository\PaymentRepositoryImpl;
+    use Repository\DetailRepositoryImpl;
     use Service\OrderServiceImpl;
     use Service\PaymentServiceImpl;
+    use Service\DetailServiceImpl;
     use View\PaymentView;
 
     function testViewShowPayment(): void 
@@ -25,9 +29,11 @@
         $connection = Database::getConnection();
         $orderRepository = new OrderRepositoryImpl($connection);
         $paymentRepository = new PaymentRepositoryImpl($connection);
+        $detailRepository = new DetailRepositoryImpl($connection);
         $orderService = new OrderServiceImpl($orderRepository);
         $paymentService = new PaymentServiceImpl($paymentRepository);
-        $paymentView = new PaymentView($orderService, $paymentService);
+        $detailService = new DetailServiceImpl($detailRepository);
+        $paymentView = new PaymentView($orderService, $paymentService, $detailService);
         $paymentView->showPayment();
     }
 
